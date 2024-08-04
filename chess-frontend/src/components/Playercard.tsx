0de871d27a60cb01f57fd37d4873e90f0aca10a8
dibkb <dibas9110@gmail.com>
2024-08-04 19:57:04 +0530

    players card render

--- chess-frontend/src/components/Playercard.tsx
+++ chess-frontend/src/components/Playercard.tsx
@@ -1,12 +1,21 @@
 import { Card, CardBody, CardHeader, Image } from "@nextui-org/react";
+import XmarkIcon from "../svg/XmarkIcon";
+import CheckIcon from "../svg/CheckIcon";
 
 export default function Playercard() {
   return (
     <Card className="py-4 w-fit">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <p className="text-tiny uppercase font-bold">Daily Mix</p>
-        <small className="text-default-500">12 Tracks</small>
-        <h4 className="font-bold text-large">Frontend Radio</h4>
+        <h4 className="font-bold text-large">Jane Doe</h4>
+        <p className="text-tiny uppercase font-bold">👑 3 win streak</p>
+        <div className="flex items-center gap-2">
+          <small className="text-default-500 clear-start flex items-center gap-1">
+            <CheckIcon className="size-5 text-lime-600" /> Wins
+          </small>
+          <small className="text-default-500 clear-start flex items-center gap-1">
+            <XmarkIcon className="size-5 text-red-600" /> Losses
+          </small>
+        </div>
       </CardHeader>
       <CardBody className="overflow-visible py-2">
         <Image
