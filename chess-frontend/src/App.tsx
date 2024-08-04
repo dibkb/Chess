ebf9d393329d097d544c31c086eec422d091649b
dibkb <dibas9110@gmail.com>
2024-08-04 19:44:58 +0530

    active players

--- chess-frontend/src/App.tsx
+++ chess-frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landingpage from "./screens/Landingpage";
 import { Join } from "./screens/Joinpage";
+import Lobby from "./screens/Lobby";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landingpage />} />
         <Route path="/join" element={<Join />} />
+        <Route path="/lobby" element={<Lobby />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ chess-frontend/src/screens/Lobby.tsx
@@ -0,0 +1,34 @@
+import { Input } from "@nextui-org/react";
+import { SearchIcon } from "../svg/SearchIcon";
+import Playercard from "../components/Playercard";
+
+export default function Lobby() {
+  return (
+    <div className="mt-4 flex flex-col gap-8">
+      <div className="flex items-center gap-4 justify-between">
+        <div className="cutive text-lg flex items-center gap-2">
+          <span className="inline-block h-3 w-3 rounded-full bg-lime-500"></span>
+          <span>14 Players Online</span>
+        </div>
+        <Input
+          classNames={{
+            base: "max-w-full sm:max-w-[32rem] h-10",
+            mainWrapper: "h-full",
+            input: "text-sm",
+            inputWrapper:
+              "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+          }}
+          placeholder="Search players..."
+          size="sm"
+          startContent={<SearchIcon size={18} />}
+          type="search"
+        />
+      </div>
+      <main className="flex flex-wrap gap-4">
+        {[...new Array(14)].map((_, idx) => (
+          <Playercard key={idx} />
+        ))}
+      </main>
+    </div>
+  );
+}
