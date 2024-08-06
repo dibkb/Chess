644dca6ab60e3d466225e096990e829c03d8f453
dibkb <dibas9110@gmail.com>
2024-08-07 02:19:19 +0530

    card ui changes

--- chess-frontend/src/components/Playercard.tsx
+++ chess-frontend/src/components/Playercard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, CardHeader, Image } from "@nextui-org/react";
+import { Button, Card, CardBody, CardHeader, Image } from "@nextui-org/react";
 import XmarkIcon from "../svg/XmarkIcon";
 import CheckIcon from "../svg/CheckIcon";
 
@@ -7,10 +7,9 @@ export default function Playercard() {
     <Card className="py-4 w-full">
       <CardHeader className="pb-0 pt-2 px-4 flex-col gap-1 items-start">
         <span className="font-bold text-large flex items-center gap-2">
-          <span className="inline-block h-2 w-2 rounded-full bg-lime-500"></span>
           Jane Doe
         </span>
-        <p className="text-tiny uppercase font-bold">👑 3 win streak</p>
+        <p className="text-tiny uppercase font-medium">👑 3 win streak</p>
         <div className="flex items-center gap-2">
           <small className="text-default-500 clear-start flex items-center gap-1">
             <CheckIcon className="size-4 text-lime-600" /> Wins
@@ -20,12 +19,15 @@ export default function Playercard() {
           </small>
         </div>
       </CardHeader>
-      <CardBody className="overflow-visible py-2">
+      <CardBody className="overflow-visible py-2 flex flex-col gap-3">
         <Image
           alt="Card background"
           className="object-cover rounded-xl"
           src="https://nextui.org/images/hero-card-complete.jpeg"
         />
+        <Button color="primary" variant="light" className="font-medium">
+          Challenge
+        </Button>
       </CardBody>
     </Card>
   );
