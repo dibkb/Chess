a0a36f114c6ce4ea5b2b1089a24553f98e1c48c0
dibkb <dibas9110@gmail.com>
2024-08-21 14:21:46 +0530

    norsemen added

--- chess-frontend/src/chess_assets/Pieces.ts
+++ chess-frontend/src/chess_assets/Pieces.ts
@@ -1,5 +1,6 @@
 import { ChessPiece, Piece } from "../types/piece";
 import { PIECES_ASTRALS } from "./astral_piece";
+import { PIECES_NORSEMEN } from "./norsemen_piece";
 import { PIECES_SPIRITS } from "./spirits_piece";
 
 type Theme = {
@@ -8,7 +9,7 @@ type Theme = {
 const GamePiece: Theme = {
   Spirits: PIECES_SPIRITS,
   Astrals: PIECES_ASTRALS,
-  Norsemen: PIECES_ASTRALS,
+  Norsemen: PIECES_NORSEMEN,
   Mythicons: PIECES_ASTRALS,
 };
 export { GamePiece };
---
+++ chess-frontend/src/chess_assets/norsemen_piece.tsx
@@ -0,0 +1,20 @@
+import { Piece } from "../types/piece";
+import { BB, BK, BN, BP, BQ, BR, WB, WK, WN, WP, WQ, WR } from "./norsemen";
+
+const PIECES_NORSEMEN: Record<Piece, React.ReactNode> = {
+  // black
+  bb: <BB />,
+  bk: <BK />,
+  bn: <BN />,
+  bp: <BP />,
+  bq: <BQ />,
+  br: <BR />,
+  // white
+  wb: <WB />,
+  wk: <WK />,
+  wn: <WN />,
+  wp: <WP />,
+  wq: <WQ />,
+  wr: <WR />,
+};
+export { PIECES_NORSEMEN };
